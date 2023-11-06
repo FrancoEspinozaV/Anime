6e0f8d0202f2bb8236c2203a4e7144390850d2d4
FrancoEspinozaV <franco.espinoza.val@gmail.com>
2023-11-06 13:05:51 -0300

    push almost done

--- Frontend/component/Nav.jsx
+++ Frontend/component/Nav.jsx
@@ -2,8 +2,18 @@ import { Link } from 'react-router-dom'
 import { supabase } from '../Supabase/supabase.config'
 import { useEffect, useState } from 'react'
 
+const ModoAdmin = () => {
+  return (
+    <li>
+      <Link to='/admin'>Modo Admin 😎</Link>
+    </li>
+  )
+}
+
 export function Nav() {
   const [link, setLink] = useState(false)
+  const [sesion, setSesion] = useState(false)
+  const [isAdmin, setIsAdmin] = useState(false)
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, session) => {
@@ -11,6 +21,40 @@ export function Nav() {
       else setLink(false)
     })
   }, [])
+
+  useEffect(() => {
+    const permisosTable = async () => {
+      try {
+        supabase.auth.onAuthStateChange((event, session) => {
+          if (session) {
+            setSesion(true)
+          } else {
+            setSesion(false)
+          }
+        })
+        if (sesion) {
+          const {
+            data: { user },
+          } = await supabase.auth.getUser()
+          if (user !== null) {
+            const { data, error } = await supabase
+              .from('Permisos')
+              .select()
+              .eq('UUID', user.id)
+
+            if (data[0].Rol === 1) {
+              setIsAdmin(true)
+            } else {
+              setIsAdmin(false)
+            }
+          }
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    permisosTable()
+  }, [sesion])
 
   return (
     <header>
@@ -25,6 +69,7 @@ export function Nav() {
           <li>
             <Link to='/search-page'>Catalogo</Link>
           </li>
+          {isAdmin ? <ModoAdmin /> : undefined}
           <li>
             {link ? (
               <Link to='/login'>Ingresar</Link>
@@ -33,6 +78,9 @@ export function Nav() {
                 to='/login'
                 onClick={() => {
                   supabase.auth.signOut()
+                  setIsAdmin(false)
+                  localStorage.setItem('isLoggedIn', false)
+                  localStorage.setItem('useRol', null)
                 }}
               >
                 Salir
--- Frontend/component/Rutas.jsx
+++ Frontend/component/Rutas.jsx
@@ -1,16 +1,46 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { Login } from '../src/Page/Login'
 import { Home } from '../src/Page/Home'
 import { SearchPage } from '../src/Page/SearchPage'
 import { Anime } from '../src/Page/Anime'
 import { NotFound } from '../src/Page/NotFound'
+import { Admin } from '../src/Page/Admin'
+import { useEffect } from 'react'
+import { useContextPage } from '../src/context/Context'
+
 export function Rutas() {
+  const { getUser, user } = useContextPage()
+  useEffect(() => {
+    getUser()
+  }, [])
+
+  // agregar un modal para verificar acciones y no
+  // mostrar nada al usuario
+  const ProtectedRoute = ({ children }) => {
+    const isLogin = localStorage.getItem('isLoggedIn')
+    if (!isLogin) return <Navigate to='/' />
+    const userRol = localStorage.getItem('useRol')
+    console.log(userRol)
+    if (userRol !== '1') {
+      return <Navigate to='/' />
+    }
+    return children
+  }
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/search-page' element={<SearchPage />} />
       <Route path='/anime/:name' element={<Anime />} />
       <Route path='/login' element={<Login />} />
+      <Route
+        path='/admin'
+        element={
+          <ProtectedRoute>
+            <Admin />
+          </ProtectedRoute>
+        }
+      />
       <Route path='*' element={<NotFound />} />
     </Routes>
   )
