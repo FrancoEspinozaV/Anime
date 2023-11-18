791a0cff964bfe810b6b05e94ad13a7e4c4b874c
FrancoEspinozaV <franco.espinoza.val@gmail.com>
2023-11-18 01:24:54 -0300

    add More Popular

--- Frontend/src/Page/Home.jsx
+++ Frontend/src/Page/Home.jsx
@@ -1,14 +1,30 @@
 import { useEffect } from 'react'
 import { useContextPage } from '../context/Context'
 import { PreIMG } from '../../component/PreIMG'
+import { PopularAnime } from '../../component/PopularAnime'
 export function Home() {
-  const { getUser, getLastChapters, lastChapters } = useContextPage()
+  const {
+    getUser,
+    getLastChapters,
+    lastChapters,
+    getMorePopularAnime,
+    morePopularAnime,
+  } = useContextPage()
   useEffect(() => {
     getLastChapters()
     getUser()
+    getMorePopularAnime()
   }, [])
   return (
     <>
+      <h2>Animes Populares</h2>
+      <ul>
+        {morePopularAnime.map((data, index) => (
+          <li key={index}>
+            <PopularAnime data={data} />
+          </li>
+        ))}
+      </ul>
       <h2>Ejemplo de como traer cosas de la base de datos</h2>
       <br />
       <ul style={{ display: 'flex', flexDirection: 'row' }}>
